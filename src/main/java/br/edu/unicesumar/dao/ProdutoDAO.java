24b9a3b89077f1e6a55d749cfbd504cdea81544f
leicam <julianoleicam@gmail.com>
2019-04-27 14:39:38 -0300

    Implementada persistencia

--- src/main/java/br/edu/unicesumar/dao/ProdutoDAO.java
+++ src/main/java/br/edu/unicesumar/dao/ProdutoDAO.java
@@ -5,19 +5,20 @@ import javax.persistence.EntityManager;
 import br.edu.unicesumar.model.Produto;
 
 public class ProdutoDAO extends DAO{
-
-
+	private EntityManager manager = getEntityManager();
+	
 	public void save(Produto produto){
-		EntityManager manager = getEntityManager();
-
 		manager.getTransaction().begin();    
 		manager.persist(produto);
 		manager.getTransaction().commit();  
 	}
 	
 	public List<Produto> list(){
-		EntityManager manager = getEntityManager();
-
 		return manager.createQuery("from Produto").getResultList();
 	}
+	
+	/*public Produto getById(Long id) {
+		
+		return manager.find(Produto, id).
+	}*/
 }
--- src/main/java/br/edu/unicesumar/rest/ProdutoService.java
+++ src/main/java/br/edu/unicesumar/rest/ProdutoService.java
@@ -4,18 +4,21 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
+import br.edu.unicesumar.dao.ProdutoDAO;
 import br.edu.unicesumar.model.Produto;
 
 @Path("/json/produto")
 public class ProdutoService {
-
+	private ProdutoDAO dao = new ProdutoDAO();
+	
 	@GET
 	@Path("/get")
-	@Produces("application/json")
-	public Produto getProdutoJSON() {
+	@Produces("application/json/{id}")
+	public Produto getProdutoJSON(@PathParam("id")Long id) {
 		Produto produto = new Produto();
 		produto.setNome("Moto X - Android");
 		produto.setQuantidade(10);
@@ -27,9 +30,10 @@ public class ProdutoService {
 	@Path("/post")
 	@Consumes("application/json")
 	public Response criarProdutoJSON(Produto produto) {
+		dao.save(produto);
+		
 		String result = "Product created : " + produto.toString();
 		return Response.status(201).entity(result).build();
 	}
-
 }
 
